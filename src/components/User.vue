ce9c3ca196e3874a71176cfb191e2a1b9d901169
Martin Kang <martin.kang@efrei.net>
2023-11-09 17:05:25 +0100

    Add User component with navigation bar

---
+++ src/components/User.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <a class="text-header" href="/">Home</a>
+    <div class="right-container">
+      <a class="text-header" href="register.html">Register</a>
+      <div style="margin-left: 30px;"></div>
+      <a class="text-header" href="login.html">Login</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  .text-header {
+    font-size: 25px;
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+  }
+
+  .text-header:hover {
+    color: #1a73e8;
+    text-decoration: underline;
+  
+  }
+  .container{
+    display: flex;
+    justify-content: space-between;
+    background-color: gray;
+    padding: 10px;
+    border: #1a73e8 2px solid;
+  }
+  .right-container {
+    display: flex;
+  }
+</style>
