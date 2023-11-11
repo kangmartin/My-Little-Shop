beb148571503509c83a99370eb842f08fa338fa1
Martin Kang <martin.kang@efrei.net>
2023-11-11 04:04:04 +0100

    Refactor App.vue to use router-view instead of individual components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import HeaderGuest from "./components/HeaderGuest.vue";
-import ProductList from './components/ProductList.vue';
+
 </script>
 
-<template>
-      <HeaderGuest/>
-      <ProductList/>
+<template>      
+      <router-view></router-view>
 </template>
 
 <style scoped>
