5e16f418094b654929ee6aa4565d4a77c22bfd28
Martin Kang <martin.kang@efrei.net>
2023-11-11 04:12:59 +0100

    Add favicon and update header design

--- src/components/HeaderGuest.vue
+++ src/components/HeaderGuest.vue
@@ -1,7 +1,13 @@
 <template>
   <header class="header">
     <nav class="nav">
-      <a class="nav__link" href="/">My Little Shop</a>
+      <div class="nav__left">
+        <a href="/">
+          <img src="favicon.ico" alt="Logo" />
+        </a>
+        <a class="nav__link" href="/">
+          My Little Shop</a>
+      </div>
       <div class="nav__right">
         <a class="nav__link" href="registration">Register</a>
         <a class="nav__link" href="login">Login</a>
@@ -9,6 +15,60 @@
     </nav>
   </header>
 </template>
+
+<style scoped>
+.header {
+  background-color: #ecf0f1;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 1rem 3%;
+  border-radius: 4px;
+  margin-left: 6%;
+  margin-right: 6%;
+  margin-top: 2%;
+  width: auto;
+}
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav__left {
+  display: flex;
+  align-items: center;
+}
+
+.nav__left img {
+  height: 30px;
+  margin-right: 10px;
+}
+
+.nav__link {
+  font-size: 1.2rem;
+  text-decoration: none;
+  color: #333;
+  margin-right: 1rem;
+  transition: color 0.2s ease-in-out;
+  font-weight: bold;
+}
+
+.nav__link:hover {
+  color: #1a73e8;
+}
+
+.nav__right {
+  display: flex;
+  align-items: center;
+}
+
+.nav__right .nav__link {
+  background-color: #1a73e8;
+  border-radius: 4px;
+  color: white;
+  padding: 4px 4px 4px 4px;
+}
+</style>
 <style scoped>
 
 .header {
