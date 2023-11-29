13c17cb81e2aad197f5485848aa8e95395a0a562
Martin Kang <martin.kang@efrei.net>
2023-11-29 22:26:37 +0100

    Add image upload functionality to productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,42 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 const { addProducts, getAllProducts } = require('../controllers/controllerProducts');
 
-
-router.post('/add', async (req, res) => {
-    try {
-        const { name, oldPrice, actualPrice, rate } = req.body;
-        addProducts(name, oldPrice, actualPrice, rate);
-        console.log('Product added successfully');
-    } catch (error) {
-       console.log(error);
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        const dest = path.join(__dirname, '../../public/images');
+        cb(null, dest);
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname); 
     }
 });
 
+const upload = multer({ storage: storage });
+
+router.post('/add', upload.single('image'), async (req, res) => {
+    try {
+        const { name, oldPrice, actualPrice, rate } = req.body;
+        const image = req.file ? req.file.filename : null;
+        await addProducts(name, image, oldPrice, actualPrice, rate);
+        console.log('Product added successfully');
+        res.status(201).send({ message: "Product added successfully" });
+    } catch (error) {
+       console.error(error);
+       res.status(500).send({ error: "Error adding product" });
+    }
+});
 
 router.get('/all', async (req, res) => {
     try {
         const products = await getAllProducts();
         res.json(products);
     } catch (error) {
-        console.log(error);
-       
+        console.error(error);
+        res.status(500).send({ error: "Error fetching products" });
     }
 });
 
-
-
 module.exports = router;
-
