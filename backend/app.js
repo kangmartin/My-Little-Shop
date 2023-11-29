62e5a14f48df80de733a22b39cc13809367ff444
Martin Kang <martin.kang@efrei.net>
2023-11-29 22:26:12 +0100

    Add multer for file upload

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
-
 app.use(cors());
+
+multer.diskStorage({
+    destination: function(req, file, cb) {
+        const dest = path.join(__dirname, '../public/images');
+        cb(null, dest);
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
 
 app.use('/api/products', productRoutes);
 
