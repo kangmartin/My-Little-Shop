b3159edfda33b83df2b35d1e28924ffd9ea7ff41
Martin Kang <martin.kang@efrei.net>
2023-12-01 04:32:16 +0100

    Add members model to backend

---
+++ backend/models/membersModel.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database/db_connexion');
+
+const Member = sequelize.define('product', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isBan: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+});
+
+
+
+
+
+module.exports = { Member, sequelize };
