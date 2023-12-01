63aa597805d8e214361e5389a6e8600ba588cd80
Martin Kang <martin.kang@efrei.net>
2023-12-01 17:05:33 +0100

    Refactor router configuration and add authentication check

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Admin from '../views/Admin.vue'
-import Cart from '../views/Cart.vue'
-import Registration from '../views/Registration.vue'
-import Login from '../views/Login.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Admin from '../views/Admin.vue';
+import Cart from '../views/Cart.vue';
+import Registration from '../views/Registration.vue';
+import Login from '../views/Login.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/admin', component: Admin },
-    { path: '/cart', component: Cart },
-    { path: '/registration', component: Registration },
-    { path: '/login', component: Login }
-
-]
+  { path: '/admin', component: Admin, meta: { requiresAuth: true } },
+  { path: '/cart', component: Cart },
+  { path: '/registration', component: Registration },
+  { path: '/login', component: Login }
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!localStorage.getItem('token')) {
+      
+      next({ path: '/login' });
+    } else {
+
+      next();
+    }
+  } else {
+   
+    next();
+  }
+});
+
+export default router;
