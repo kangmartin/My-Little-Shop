19b6af03261ab647a6216386fb6a4964979044e2
Martin Kang <martin.kang@efrei.net>
2023-12-03 02:29:54 +0100

    Remove unnecessary import statement

--- backend/controllers/controllerProducts.js
+++ backend/controllers/controllerProducts.js
@@ -1,4 +1,4 @@
-const { Product, sequelize } = require('../models/productsModel');
+const { Product } = require('../models/productsModel');
 
 
 async function addProducts(name, image, oldPrice, actualPrice, rate) {
