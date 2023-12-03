2d565d9a76bb6d19020d3138e846f36ccbec3a5b
Martin Kang <martin.kang@efrei.net>
2023-12-03 07:19:55 +0100

    Add AdminView component with header and manage members functionality

---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <h2 class="user">Users</h2>
+    <div class="form-manage-container">
+      <ManageMembers/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from "../components/HeaderComponent.vue";
+  import ManageMembers from "../components/ManageMembers.vue";
+
+
+  export default {
+    components: {
+      HeaderComponent,
+      ManageMembers
+    },
+  }
+</script>
+<style>
+.form-manage-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 30px;
+}
+
+.user {
+ font-size: 1.5rem;
+ color: white;
+ text-decoration: underline;
+ margin-bottom: 20px;
+ text-align: center;
+}
+
+
+@media (max-width: 768px) {
+  .form-manage-container {
+    flex-direction: column;
+  }
+}
+</style>
+
