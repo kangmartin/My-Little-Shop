177dd6403aee28e425b235ea58406a066125cdfc
Martin Kang <martin.kang@efrei.net>
2023-12-03 01:52:43 +0100

    Fix import path in membersModel.js

--- backend/models/membersModel.js
+++ backend/models/membersModel.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../database/db_connexion');
+const { sequelize } = require('../db_connexion');
 
 const Member = sequelize.define('member', {
     id: {
