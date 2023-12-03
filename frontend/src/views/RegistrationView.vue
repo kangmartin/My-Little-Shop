7c4175ce27b1e57bcf4d864b441e3f54cfcb7b82
Martin Kang <martin.kang@efrei.net>
2023-12-03 07:20:12 +0100

    Add RegistrationView component

---
+++ frontend/src/views/RegistrationView.vue
@@ -0,0 +1,16 @@
+<script>
+    import HeaderComponent from "../components/HeaderComponent.vue";
+    import SignupForm from "../components/SignupForm.vue";
+    export default {
+  components: {
+    HeaderComponent,
+    SignupForm,
+  }
+}
+</script>
+<template>
+    
+        <HeaderComponent />
+        <SignupForm />
+       
+</template>
