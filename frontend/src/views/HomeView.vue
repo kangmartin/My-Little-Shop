1ab5ad04902b099b084cab4c05ab47bc9431354e
Martin Kang <martin.kang@efrei.net>
2023-12-03 07:20:02 +0100

    Add HomeView component with HeaderComponent and ProductList

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+    import ProductList from "../components/ProductList.vue";
+    import HeaderComponent from "../components/HeaderComponent.vue";
+    export default {
+  components: {
+    HeaderComponent,
+    ProductList,
+  }
+}
+</script>
+<template>
+    
+        <HeaderComponent />
+        <ProductList />
+
+</template>
