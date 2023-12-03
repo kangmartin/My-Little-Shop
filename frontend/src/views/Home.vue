972b367d5c67039aaa7ac8cb4e8107a5959c26dd
Martin Kang <martin.kang@efrei.net>
2023-12-03 02:30:36 +0100

    Remove unnecessary imports from Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,10 @@
 <script>
     import ProductList from "../components/ProductList.vue";
     import HeaderComponent from "../components/HeaderComponent.vue";
-    import SignupForm from "../components/SignupForm.vue";
-    import LoginForm from "../components/LoginForm.vue";
-    import CartComponent from "../components/CartComponent.vue";
     export default {
   components: {
     HeaderComponent,
     ProductList,
-    SignupForm,
-    LoginForm,
-    CartComponent
   }
 }
 </script>
