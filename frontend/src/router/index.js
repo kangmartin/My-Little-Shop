31bab8ba126a0f81f0b35107905bdc399d2811ad
Martin Kang <martin.kang@efrei.net>
2023-12-03 02:30:25 +0100

    Refactor authentication and authorization logic in router index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,11 +4,11 @@ import Admin from '../views/Admin.vue';
 import Cart from '../views/Cart.vue';
 import Registration from '../views/Registration.vue';
 import Login from '../views/Login.vue';
-
+import { jwtDecode } from "jwt-decode";
 const routes = [
   { path: '/', component: Home },
-  { path: '/admin', component: Admin, meta: { requiresAuth: true } },
-  { path: '/cart', component: Cart, meta: { requiresAuth: true } },
+  { path: '/admin', component: Admin, meta: { requiresAuth: true, requiredRole: 'admin' } },
+  { path: '/cart', component: Cart, meta: { requiresAuth: true , requiredRole: 'user' } },
   { path: '/registration', component: Registration },
   { path: '/login', component: Login }
 ];
@@ -18,17 +18,31 @@ const router = createRouter({
   routes
 });
 
+function decodeJWT(token) {
+  try {
+    const decoded = jwtDecode(token);
+    const role = decoded.user.role;
+    return { decoded, role };
+  } catch (error) {
+    console.error('Error of decode JWT', error);
+    return null;
+  }
+}
+
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!localStorage.getItem('token')) {
-      
+    const token = localStorage.getItem('token');
+    if (!token) {
       next({ path: '/login' });
     } else {
-
-      next();
+      const userInfo = decodeJWT(token);
+      if (to.meta.requiredRole && userInfo.role !== to.meta.requiredRole) {
+        next({ path: '/' });
+      } else {
+        next();
+      }
     }
   } else {
-   
     next();
   }
 });
